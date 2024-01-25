654ec9889d98ad77f531e958e040e10f690eae07
fatenaldousari <fatennald@gmail.com>
2024-01-25 11:55:46 +0300

    details page

--- app/src/main/java/com/example/branchlistproject/composables/App.kt
+++ app/src/main/java/com/example/branchlistproject/composables/App.kt
@@ -1,26 +1,56 @@
 package com.example.branchlistproject.composables
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.branchlistproject.R
 import com.example.branchlistproject.ui.theme.DarkGreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun App(modifier: Modifier = Modifier){
-    Scaffold(modifier = modifier.fillMaxSize(), topBar = {
-        TopAppBar(title = { Text(text = "AppBar")},
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = DarkGreen,
-                titleContentColor = Color.White)
-        )}) {
-            BranchNavHost(modifier = Modifier.padding(it))
+fun App(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically){
+                        Text(text = "Kuwait Finance House")
+                        Image(
+                            painter = painterResource(id =R.drawable.logo),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(50.dp)
+                                .clip(shape = MaterialTheme.shapes.small),
+
+
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = DarkGreen,
+                    titleContentColor = Color.White
+                )
+            )
+        }) {
+        BranchNavHost(modifier = Modifier.padding(it))
     }
 }
